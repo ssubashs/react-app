c90b85e9a5fade2564ef4ef8a17a29d93ea5846d
Subash Soundrapandi <ssoundrapandi@rubiconproject.com>
2018-09-27 18:39:25 -0700

    initial

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import library_data from './library';
+const es6_constants = 'es6_constants';
+console.log(`value from library ${library_data}`);
+console.log(`value from es6 constants ${es6_constants}`);
